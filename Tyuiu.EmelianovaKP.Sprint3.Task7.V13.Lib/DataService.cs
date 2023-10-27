10a7649b856f6dcfb106d69f5039fe2abceee688
fivifri <emelianova.kp@phystech.edu>
2023-10-27 20:20:45 +0500

    Task7

---
+++ Tyuiu.EmelianovaKP.Sprint3.Task7.V13.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.EmelianovaKP.Sprint3.Task7.V13.Lib
+{
+    public class DataService : ISprint3Task7V13
+    {
+        public double Func(int x)
+        { 
+            if (Math.Cos(x) + 1 == 0)
+            {
+                return 0.00;
+            }
+
+            return Math.Round(3 * x + 2 - (2.0 * x - x) / (Math.Cos(x) + 1), 2);
+        }
+
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] result = new double[stopValue - startValue + 1];
+
+            int id = 0;
+
+            for (int i = startValue; i <= stopValue; ++i)
+            {
+                result[id++] = Func(i);
+            } 
+
+            
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.EmelianovaKP.Sprint3.Task7.V13.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.EmelianovaKP.Sprint3.Task7.V13.Lib;
+
+namespace Tyuiu.EmelianovaKP.Sprint3.Task7.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double[] res = {-9.10,  1.55, 292.78, -0.57, -0.35, 2.00, 4.35, 4.57, -288.78, 2.45, 13.10};
+
+            CollectionAssert.AreEqual(res, ds.GetMassFunction(-5, 5));
+        }
+    }
+}
